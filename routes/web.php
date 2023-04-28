e6feaaaca4e872b6ea9622f2802cb88fd6f6f60e
alejandrodev2021 <favian.avila@full-assistance.net>
2023-04-27 22:34:26 -0400

    Project Inital

---
+++ routes/web.php
@@ -0,0 +1,154 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::group(['middleware'=>['guest']],function(){
+    Route::get('/','Auth\LoginController@showLoginForm');
+    Route::post('/login', 'Auth\LoginController@login')->name('login');
+});
+
+Route::group(['middleware'=>['auth']],function(){
+    
+    Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+    
+    Route::get('/main', function () {
+        return view('contenido/contenido');
+    })->name('main');
+
+    // Route::get('/pdf', function () {
+    //     return view('templatesPdf/OverviewStore');
+    // })->name('main');
+
+    // Route::get('/pdf-compra', function () {
+        
+    // })->name('main');
+
+    // Route::get('/libros', function () {
+    //     return view('welcome');
+    // });
+
+    Route::group(['middleware' => ['Almacenero']], function () {
+        Route::get('/categoria', 'CategoriaController@index');
+        Route::post('/categoria/registrar', 'CategoriaController@store');
+        Route::put('/categoria/actualizar', 'CategoriaController@update');
+        Route::put('/categoria/desactivar', 'CategoriaController@desactivar');
+        Route::put('/categoria/activar', 'CategoriaController@activar');
+        Route::get('/categoria/selectCategoria', 'CategoriaController@selectCategoria');
+
+        Route::get('/articulo', 'ArticuloController@index');
+        Route::post('/articulo/registrar', 'ArticuloController@store');
+        Route::put('/articulo/actualizar', 'ArticuloController@update');
+        Route::put('/articulo/desactivar', 'ArticuloController@desactivar');
+        Route::put('/articulo/activar', 'ArticuloController@activar');
+        Route::get('/articulo/buscarArticulo', 'ArticuloController@buscarArticulo');
+        Route::get('/articulo/listarArticulo', 'ArticuloController@listarArticulo'); 
+
+        Route::get('/proveedor', 'ProveedorController@index');
+        Route::post('/proveedor/registrar', 'ProveedorController@store');
+        Route::put('/proveedor/actualizar', 'ProveedorController@update');
+        Route::get('/proveedor/selectProveedor', 'ProveedorController@selectProveedor');
+        
+        
+        Route::get('/ingreso', 'IngresoController@index');
+        Route::get('/ingreso/overview', 'IngresoController@overview');
+        Route::post('/ingreso/registrar', 'IngresoController@store');
+        Route::put('/ingreso/desactivar', 'IngresoController@desactivar');
+        Route::put('/ingreso/editar', 'IngresoController@editar');
+
+    });
+
+    Route::group(['middleware' => ['Vendedor']], function () {
+        Route::get('/cliente', 'ClienteController@index');
+        Route::post('/cliente/registrar', 'ClienteController@store');
+        Route::put('/cliente/actualizar', 'ClienteController@update');
+        Route::get('/cliente/selectCliente', 'ClienteController@selectCliente');
+        Route::get('/articulo/buscarArticuloVenta', 'ArticuloController@buscarArticuloVenta');
+        Route::get('/articulo/listarArticuloVenta', 'ArticuloController@listarArticuloVenta');
+
+
+
+        Route::get('/venta', 'VentaController@index');
+        Route::post('/venta/registrar', 'VentaController@store');
+        Route::put('/venta/desactivar', 'VentaController@desactivar');
+        Route::get('/venta/obtenerCabecera', 'VentaController@obtenerCabecera');
+        Route::get('/venta/obtenerDetalles', 'VentaController@obtenerDetalles'); 
+    });
+
+    Route::group(['middleware' => ['Administrador']], function () {
+        
+        Route::get('/categoria', 'CategoriaController@index');
+        Route::post('/categoria/registrar', 'CategoriaController@store');
+        Route::put('/categoria/actualizar', 'CategoriaController@update');
+        Route::put('/categoria/desactivar', 'CategoriaController@desactivar');
+        Route::put('/categoria/activar', 'CategoriaController@activar');
+        Route::get('/categoria/selectCategoria', 'CategoriaController@selectCategoria');
+
+        Route::get('/articulo', 'ArticuloController@index');
+        Route::post('/articulo/registrar', 'ArticuloController@store');
+        Route::put('/articulo/actualizar', 'ArticuloController@update');
+        Route::put('/articulo/desactivar', 'ArticuloController@desactivar');
+        Route::put('/articulo/activar', 'ArticuloController@activar');
+        Route::get('/articulo/buscarArticulo', 'ArticuloController@buscarArticulo');
+        Route::get('/articulo/listarArticulo', 'ArticuloController@listarArticulo');
+        Route::get('/articulo/buscarArticuloVenta', 'ArticuloController@buscarArticuloVenta');
+        Route::get('/articulo/listarArticuloVenta', 'ArticuloController@listarArticuloVenta');
+
+        Route::get('/proveedor', 'ProveedorController@index');
+        Route::post('/proveedor/registrar', 'ProveedorController@store');
+        Route::put('/proveedor/actualizar', 'ProveedorController@update');
+        Route::get('/proveedor/selectProveedor', 'ProveedorController@selectProveedor');
+
+        Route::get('/ingreso', 'IngresoController@index');
+        Route::get('/ingreso/overview', 'IngresoController@overview');
+        Route::post('/ingreso/registrar', 'IngresoController@store');
+        Route::put('/ingreso/desactivar', 'IngresoController@desactivar');
+        
+        
+        Route::get('/cliente', 'ClienteController@index');
+        Route::post('/cliente/registrar', 'ClienteController@store');
+        Route::put('/cliente/actualizar', 'ClienteController@update');
+        Route::get('/cliente/selectCliente', 'ClienteController@selectCliente');
+
+        Route::get('/venta', 'VentaController@index');
+        Route::get('/venta/obtener-chofer', 'VentaController@obtenerChofer');
+        Route::post('/venta/registrar', 'VentaController@store');
+        Route::put('/venta/editar', 'VentaController@editar');
+        Route::put('/venta/desactivar', 'VentaController@desactivar');
+        Route::get('/venta/obtenerCabecera', 'VentaController@obtenerCabecera');
+        Route::get('/venta/obtenerDetalles', 'VentaController@obtenerDetalles');
+        
+
+        Route::get('/rol', 'RolController@index');
+        Route::get('/rol/selectRol', 'RolController@selectRol');
+        
+        Route::get('/user', 'UserController@index');
+        Route::post('/user/registrar', 'UserController@store');
+        Route::put('/user/actualizar', 'UserController@update');
+        Route::put('/user/desactivar', 'UserController@desactivar');
+        Route::put('/user/activar', 'UserController@activar');
+
+        Route::get('/pdf', 'PdfController@index');
+        Route::get('/pdf-termic', 'PdfController@indexTermic');
+        Route::get('/pdf-compra', 'PdfIngresoController@index');
+        Route::get('/pdf-compra-termic', 'PdfIngresoController@indexTermic');
+        Route::get('/report-venta', 'PdfController@index2');
+        Route::get('/report-venta-unit', 'PdfController@currentStoreId');
+        Route::get('/report-venta-termic', 'PdfTermicController@currentStoreId');
+        Route::get('/report-compra','PdfIngresoController@index2');
+        Route::get('/report-compra-unit','PdfIngresoController@currentStoreId');
+        Route::get('/report-compra-unit-termic','PdfIngresoController@currentTermicStoreId');
+    });
+
+});
+
+//Route::get('/home', 'HomeController@index')->name('home');
